4ddb414ab04b8a4f856f330c05954e64e72d9424
Okamii <2314120h@student.gla.ac.uk>
2021-08-29 19:44:52 +0100

    implemented floyds algorithm

--- src/rabbit-hare.c
+++ src/rabbit-hare.c
@@ -3,10 +3,10 @@
 struct node
 {
    int value = NULL;
-   int * next = NULL;
+   struct node *next = NULL;
 };
 
-struct node * floyd(struct node * head)
+struct node *floyd(struct node *head)
 {
    // Initialise two pointers (tortoise and hare) that both point to the head of the linked list
    struct node *tortoise;
@@ -18,20 +18,35 @@ struct node * floyd(struct node * head)
       return NULL;
    }
 
+   tortoise = head;
+   hare = head;
+
    // Loop as long as the hare does not reach null
+   while ((hare->next != NULL) && (hare->next->next != NULL))
+   {
+      // Set tortoise to next node
+      tortoise = tortoise->next;
+      // Set hare to next, next node
+      hare = hare->next->next;
 
-   // Set tortoise to next node
+      // If they are at the same node, reset the tortoise back to the head
+      if (tortoise == hare)
+      {
+         tortoise = head;
 
-   // Set hare to next, next node
+         // Have both tortoise and hare both move one node at a time until they meet again
+         while (tortoise != hare)
+         {
+            tortoise = tortoise->next;
+            hare = hare->next;
+         }
 
-   // If they are at the same node, reset the tortoise back to the head
-
-   // Have both tortoise and hare both move one node at a time until they meet again
-
-   // Return the node in which they meet
+         // Return the node in which they meet
+         return (hare);
+      }
+   }
 
    // Else, if the hare reaches null, then return null
-
    return NULL;
 }
 
